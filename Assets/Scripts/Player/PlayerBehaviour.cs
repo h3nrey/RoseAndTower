e7460d1c72099180148162c926bedfd058759900
pedorooo <150040569+pedorooo@users.noreply.github.com>
2024-06-16 21:13:59 -0300

    feat: importing input sysyem and naughty attributes

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private Player _data;
+    // Components
+    private Rigidbody rb;
+}

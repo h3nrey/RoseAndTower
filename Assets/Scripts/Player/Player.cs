ee4d4c1e7fdfd8b42bfa9432cce32b34baae2617
pedorooo <150040569+pedorooo@users.noreply.github.com>
2024-06-16 21:48:06 -0300

    feat: iniciando a movimentação

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "player/new")]
 public class Player : ScriptableObject
 {
     public float velocity;
     public float baseJumpForce;
+    public float fallingSpeed;
 }
--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,10 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] private Player _data;
+    [SerializeField] [Expandable] private Player _data;
+
+    [SerializeField] private Vector2 input;
+
+    [SerializeField] private UnityEvent onJump;
     // Components
-    private Rigidbody rb;
+    private Rigidbody _rb;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody>();
+
+        onJump.AddListener(DoJump);
+    }
+    private void FixedUpdate() {
+        _rb.velocity = new(input.x * _data.velocity * Time.fixedDeltaTime, _rb.velocity.y);
+
+        if(_rb.velocity.y < 0) {
+            _rb.velocity = new(_rb.velocity.x, Mathf.Min(_rb.velocity.y * 0.5f, _data.fallingSpeed), _rb.velocity.z);
+        }
+    }
+
+    public void GetMove(InputAction.CallbackContext context) {
+        input = context.ReadValue<Vector2>();
+    }
+
+    public void GetJump(InputAction.CallbackContext context) {
+        if(context.started) {
+            onJump.Invoke();
+        }
+    }
+
+    private void DoJump() {
+        _rb.velocity = Vector2.up * _data.baseJumpForce;
+    }
 }
